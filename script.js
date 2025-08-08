4997ea5d3d8501e72b887e5ab14fe715cc99bc43
tonmoyjs6 <tonmoybasak20567@gmail.com>
2025-08-08 13:42:59 +0600

    last commited

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// aboutus-chefs
 let count=0
 let target=15
 
@@ -11,7 +11,7 @@ let interval=setInterval(()=>{
             clearInterval(interval)
        }
     
-},100)
+},400)
 
 
 let chefsCount=0
@@ -28,13 +28,73 @@ let shefsInterval=setInterval(()=>{
         clearInterval(shefsInterval)
     }
 
-},100)
+},300)
 
 
 
 
 // api for food menu
+
+
 let mainContainer=document.getElementById("main-items")
+
+const breakfast=document.getElementById("breakfast")
+
+breakfast.addEventListener("click",()=>{
+    mainContainer.innerHTML=""
+
+
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    .then(values=>values.json())
+    .then(values=>{
+        const realValues=values.categories.slice(0,6)
+         const mainItems=document.getElementById("main-items")
+        realValues.map((element)=>{
+            console.log(element);
+   
+
+            // const menuItems=document.getElementById("items")
+            const div=document.createElement("div")
+            div.id="Items"
+
+            mainItems.appendChild(div)
+            let image=document.createElement("img")
+            image.src=element.strCategoryThumb
+            div.appendChild(image)
+            let h4=document.createElement("h4")
+            h4.innerText=`${element.strCategoryDescription.slice(0,40)}`
+            div.appendChild(h4)
+            let p=document.createElement("p")
+            p.innerText="$115"
+            p.style.color="#E1AD01"
+            p.style.fontSize="40px"
+            div.appendChild(p)
+
+
+
+
+
+
+            
+            
+            
+
+        })
+    })
+   
+  
+
+   
+})
+
+
+
+
+
+
+// lunch
+
+
 
 const lunch=document.getElementById("lunch")
 
@@ -86,6 +146,9 @@ lunch.addEventListener("click",()=>{
    
 })
 
+
+
+// dinner
 
 let dinner=document.getElementById("dinner")
 
